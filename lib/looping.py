3ce7dad1801ae71f975e47209b1755b545484868
ckorir <chrisruto.k@gmail.com>
2023-11-21 14:37:29 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+
+    square_integers = [int*int for int in int_list]
+    return square_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print(num)    
